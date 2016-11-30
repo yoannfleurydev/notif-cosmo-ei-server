d0376ed7c6cdd445811d45acca5d6620f0938125
Yoann Fleury <yoann.fleury@yahoo.com>
2016-11-30 10:16:53 +0100

    feat: init repository

---
+++ src/main/java/eu/yoannfleury/NotifCosmoEiServerApplication.java
@@ -0,0 +1,12 @@
+package eu.yoannfleury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifCosmoEiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifCosmoEiServerApplication.class, args);
+	}
+}
